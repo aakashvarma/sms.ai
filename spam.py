571657b21c88d01bdd0c63410b31f4faf1638826
aakashvarma18 <aakashvarma18@gmail.com>
2018-06-01 21:14:16 +0530

    new libraries added

--- spam.py
+++ spam.py
@@ -1,12 +1,21 @@
-import os
-import csv
+import os, csv, re, nltk
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import confusion_matrix
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+import snowballstemmer
 
 
 class spam():
@@ -27,28 +36,23 @@ class spam():
             le = preprocessing.LabelEncoder()
             le.fit(df['v1'])
             df['v1'] = le.transform(df['v1'])
-
-
-            # X_train, X_test, y_train, y_test = train_test_split(df['v2'], df['v1'], test_size=0.33, random_state=42)
-            # print (y_train)
-
+            print (df.shape)
 
             self.df = df
-            # print (self.df.head())
 
         except IOError:
             print ('PROBLEM READING: ' + filename)
 
     def data_split(self):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.df['v2'], self.df['v1'], test_size=0.33, random_state=42)
-        # print (np.array(self.y_train))
 
     def vectorizer(self):
         count_vect = CountVectorizer()
         X_train_counts = count_vect.fit_transform(np.array(self.X_train))
         tfidf_transformer = TfidfTransformer()
         X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-        print(X_train_tfidf.shape)
+        print (X_train_counts.shape)
+        print (X_train_tfidf.shape)
 
 
 
